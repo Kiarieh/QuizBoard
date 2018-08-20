6425b4915efbabd488aeccfecde4dcdb80beda41
antony <kiariehjames@gmail.com>
2018-08-20 11:12:09 +0300

    Add click button to display values

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 var quizAnswers = function(one, two, three, four, five){
-return one + two + three + four + five;
+return parseInt(one) + parseInt(two) + parseInt(three) + parseInt(four) + parseInt(five);
 }
 
 $(document).ready(function(){
-  });
 
   $("form#quizy").submit(function(event){
-    var number1 = parseInt($("input:radio[name=num1]:checked").val());
-    var number2 = parseInt($("input:radio[name=num2]:checked").val());
-    var number3 = parseInt($("input:radio[name=num3]:checked").val());
-    var number4 = parseInt($("input:radio[name=num4]:checked").val());
-    var number5 = parseInt($("input:radio[name=num5]:checked").val());
-    var result = quizAnswers(number1, number2, number3, number4, number5);
+    var nameDisplay =$
+    var number1 = $("input:radio[name=num1]:checked").val();
+    var number2 = $("input:radio[name=num2]:checked").val();
+    var number3 = $("input:radio[name=num3]:checked").val();
+    var number4 = $("input:radio[name=num4]:checked").val();
+    var number5 = $("input:radio[name=num5]:checked").val();
+    var result = quizAnswers(number1,number2,number3,number4,number5);
+    $("#shows").text(nameDisplay + "Your total score is:" + result + "out of 10");
 
-    var finals = $("#shows").text("#name"  + "Your total score is" + result + "out of 10");
-
-    event.preventDefault();
+    $("#quizy").hide();
     $(".scores").show();
-    alert (finals)
+    event.preventDefault();
+
   });
 });
