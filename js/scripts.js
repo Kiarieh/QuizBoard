2d0f7c6ade1d24d51daac62ecdc9d57a1dbf14cf
antony <kiariehjames@gmail.com>
2018-08-20 11:38:22 +0300

    Add color to name textbox'

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,15 @@ return parseInt(one) + parseInt(two) + parseInt(three) + parseInt(four) + parseI
 $(document).ready(function(){
 
   $("form#quizy").submit(function(event){
-    var nameDisplay =$
+    var nameDisplay =$("input#name").val();
     var number1 = $("input:radio[name=num1]:checked").val();
     var number2 = $("input:radio[name=num2]:checked").val();
     var number3 = $("input:radio[name=num3]:checked").val();
     var number4 = $("input:radio[name=num4]:checked").val();
     var number5 = $("input:radio[name=num5]:checked").val();
     var result = quizAnswers(number1,number2,number3,number4,number5);
-    $("#shows").text(nameDisplay + "Your total score is:" + result + "out of 10");
+    $("#name").append(nameDisplay);
+    $("#shows").text(nameDisplay  + "Your total score is:" + result + "out of 10");
 
     $("#quizy").hide();
     $(".scores").show();
